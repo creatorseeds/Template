9531c1e240e462ad4b791205e79a9974aa60db19
Tsubasa Homma <hello@creatorseeds.com>
2015-04-22 06:35:12 +0900

    Rename components directory.

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "components/css"
-sass_dir = "components/sass"
-images_dir = "components/img"
-javascripts_dir = "components/js"
+css_dir = "assets/css"
+sass_dir = "assets/sass"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -22,4 +22,4 @@ line_comments = false
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
-# sass-convert -R --from scss --to sass components/sass scss && rm -rf sass && mv scss sass
+# sass-convert -R --from scss --to sass assets/sass scss && rm -rf sass && mv scss sass
